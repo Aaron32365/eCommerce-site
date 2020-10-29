dca087f2cbe7e1d2dd01b8e3cc599aa01853a249
Aaron32365 <56372224+Aaron32365@users.noreply.github.com>
2020-10-29 02:01:19 -0700

    added __dirname to server file

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ import userRoute from "./routes/userRoute.js"
 import bodyParser from 'body-parser'
 import productRoute from "./routes/productRoute.js"
 import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenv.config()
 const mongodbUrl = config.MONGODB_URL
 
